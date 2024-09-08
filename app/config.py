d29ed8cc6468c1dfc06b03b4dae329a0941237d3
timf34 <farrelti@tcd.ie>
2024-09-08 23:43:26 +0100

    updated wildcards in mqtt topics

--- app/config.py
+++ app/config.py
@@ -1,16 +1,12 @@
-import os
-
-USE_OMAR_CERTS: bool = False
-
-
-class MQTTMergerConfig:
+class FOVDashboardConfig:
     def __init__(self):
 
-        self.version_topic: str = "ap-southeast-2/marvel/fov-marvel-+/version"
-        self.battery_topic: str = "ap-southeast-2/marvel/fov-marvel-+/battery"
-        self.temperature_topic: str = "ap-southeast-2/marvel/fov-marvel-+/temperature"
+        # Wild card docs: https://docs.aws.amazon.com/iot/latest/developerguide/topics.html#topicfilters
+        self.version_topic: str = "ap-southeast-2/marvel/+/version"
+        self.battery_topic: str = "ap-southeast-2/marvel/+/battery"
+        self.temperature_topic: str = "ap-southeast-2/marvel/+/temperature"
 
-        self.endpoint: str = "a13d7wu4wem7v1-ats.iot.ap-southeast-2.amazonaws.com"
+        self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.eu-west-1.amazonaws.com"
         self.cert_path: str = "./aws-iot-certs/fov-dashboard-client-dublin-1-certificate.pem.crt"
         self.private_key_path: str = "./aws-iot-certs/fov-dashboard-client-dublin-1-private.pem.key"
         self.root_ca_path: str = "./aws-iot-certs/AmazonRootCA1.pem"

c5df4aedcf00448cb04a891ac932c2bb8e730480
timf34 <farrelti@tcd.ie>
2024-09-09 02:01:51 +0100

    adding tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust this path to match your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

284e203122e66f7ce4038514dcbf1d1ccd233694
timf34 <farrelti@tcd.ie>
2024-09-12 17:08:32 +0100

    updated to australian certs

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class FOVDashboardConfig:
         self.battery_topic: str = "ap-southeast-2/marvel/+/battery"
         self.temperature_topic: str = "ap-southeast-2/marvel/+/temperature"
 
-        self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.eu-west-1.amazonaws.com"
-        self.cert_path: str = "./aws-iot-certs/fov-dashboard-client-dublin-1-certificate.pem.crt"
-        self.private_key_path: str = "./aws-iot-certs/fov-dashboard-client-dublin-1-private.pem.key"
-        self.root_ca_path: str = "./aws-iot-certs/AmazonRootCA1.pem"
+        self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.ap-southeast-2.amazonaws.com"
+        self.cert_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/fov-dashboard-client-sydney-1-certificate.pem.crt"
+        self.private_key_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/fov-dashboard-client-sydney-1-private.pem.key"
+        self.root_ca_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/fov-dashboard-client-sydney-1-public.pem.key"

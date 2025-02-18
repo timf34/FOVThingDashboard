f8295d44cbbabd93d3cbf2a73d1c7001a0967b77
timf34 <farrelti@tcd.ie>
2025-02-18 00:50:51 +0000

    updated config.py to irish connection

--- app/config.py
+++ app/config.py
@@ -2,17 +2,22 @@ class FOVDashboardConfig:
     def __init__(self):
 
         # Wild card docs: https://docs.aws.amazon.com/iot/latest/developerguide/topics.html#topicfilters
-        self.version_topic: str = "ap-southeast-2/marvel/+/version"
-        self.battery_topic: str = "ap-southeast-2/marvel/+/battery"
-        self.temperature_topic: str = "ap-southeast-2/marvel/+/temperature"
-        self.ota_topic: str = "ap-southeast-2/marvel/+/ota"
+        # self.version_topic: str = "ap-southeast-2/marvel/+/version"
+        # self.battery_topic: str = "ap-southeast-2/marvel/+/battery"
+        # self.temperature_topic: str = "ap-southeast-2/marvel/+/temperature"
+        # self.ota_topic: str = "ap-southeast-2/marvel/+/ota"
 
-        self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.ap-southeast-2.amazonaws.com"
-        self.cert_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/fov-dashboard-client-sydney-1-certificate.pem.crt"
-        self.private_key_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/fov-dashboard-client-sydney-1-private.pem.key"
-        self.root_ca_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/AmazonRootCA1.pem"
+        # self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.ap-southeast-2.amazonaws.com"
+        # self.cert_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/fov-dashboard-client-sydney-1-certificate.pem.crt"
+        # self.private_key_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/fov-dashboard-client-sydney-1-private.pem.key"
+        # self.root_ca_path: str = "./aws-iot-certs/fov-dashboard-sydney-client/AmazonRootCA1.pem"
 
-        # self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.eu-west-1.amazonaws.com"
-        # self.cert_path: str = "./aws-iot-certs/fov-dashboard-dublin-client/certificate.pem.crt"
-        # self.private_key_path: str = "./aws-iot-certs/fov-dashboard-dublin-client/private.pem.key"
-        # self.root_ca_path: str = "./aws-iot-certs/fov-dashboard-dublin-client/AmazonRootCA1"
+        self.version_topic: str = "eu-west-1/aviva/+/version"
+        self.battery_topic: str = "eu-west-1/aviva/+/battery"
+        self.temperature_topic: str = "eu-west-1/aviva/+/temperature"
+        self.ota_topic: str = "eu-west-1/aviva/+/ota"
+
+        self.endpoint: str = "a3lkzcadhi1yzr-ats.iot.eu-west-1.amazonaws.com"
+        self.cert_path: str = "./aws-iot-certs/fov-dashboard-dublin-client/certificate.pem.crt"
+        self.private_key_path: str = "./aws-iot-certs/fov-dashboard-dublin-client/private.pem.key"
+        self.root_ca_path: str = "./aws-iot-certs/fov-dashboard-dublin-client/AmazonRootCA1"
